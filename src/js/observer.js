34f2ca3dfefa251cb92bdd85d6de580b55561806
Joenz Kim-Chan <jokichaa@gmail.com>
2024-05-30 23:18:56 +0800

    chore: remove console log, scroll snapping test

--- src/js/observer.js
+++ src/js/observer.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("isVisible");
     } else {
